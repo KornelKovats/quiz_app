e515ee1f4c37f7aeb161fd2fa6132eef9fe9d576
KornelKovats <kovats.kornel95@gmail.com>
2020-11-14 17:19:03 +0100

    made for heroku

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(router);
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.redirect('/game');
