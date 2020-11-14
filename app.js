d92fa8c603e38dcb9653d62b933eec847f42384f
KornelKovats <kovats.kornel95@gmail.com>
2020-11-14 17:36:13 +0100

    changed source

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(router);
 
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.redirect('/game');
