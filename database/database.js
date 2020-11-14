e7ef48d1e8067ea0a66e17899a45eb4922162d90
KornelKovats <kovats.kornel95@gmail.com>
2020-11-14 18:13:54 +0100

    added heroku link to readme

--- database/database.js
+++ database/database.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
 const sqlQuery = (sql, args) => new Promise((resolve, reject) => {
-  db.query(sql, args, (err, data) => {
+  pool.query(sql, args, (err, data) => {
     if (err) { reject(err); console.error(err.sqlMessage); }
     resolve(data);
   });
